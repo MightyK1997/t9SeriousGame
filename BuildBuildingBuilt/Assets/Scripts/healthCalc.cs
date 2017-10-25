f26f0a058224a6b087807fae823d8119fdbe8663
MightyK1997 <31252492+MightyK1997@users.noreply.github.com>
2017-10-25 17:12:27 -0600

    Added the json file and made some changes to the code..

---
+++ BuildBuildingBuilt/Assets/Scripts/healthCalc.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class healthCalc : MonoBehaviour {
+
+    public static string houseHealth;
+    int roofHealth;
+    int foundationHealth;
+    int wall1Health;
+    int wall2Health;
+    int supportHealth;
+    string path;
+    Texture[] allTextures;
+    List<TextureHealth> tHealthList = new List<TextureHealth>();
+	// Use this for initialization
+	void Start () {
+        allTextures = Resources.FindObjectsOfTypeAll(typeof(Texture)) as Texture[];
+        path = Application.streamingAssetsPath + "/TextureHealth.json";
+        string jsonString = File.ReadAllText(path);
+        TextureHealth tHealth = JsonUtility.FromJson<TextureHealth>(jsonString);
+        tHealthList.Add(tHealth);
+        foreach (var item in tHealthList)
+        {
+            //foreach (var texture in allTextures)
+            //{
+            //    if (item.name == texture.name)
+            //    {
+            //        if (texture.name.Contains("roof"))
+            //        {
+            //            roofHealth = item.health;
+            //        }
+            //        if (texture.name.Contains("foundation"))
+            //        {
+            //            foundationHealth = item.health;
+            //        }
+            //        if (texture.name.Contains("wall1"))
+            //        {
+            //            wall1Health = item.health;
+            //        }
+            //        if (texture.name.Contains("wall2"))
+            //        {
+            //            wall2Health = item.health;
+            //        }
+            //        if (texture.name.Contains("support"))
+            //        {
+            //            supportHealth = item.health;
+            //        }
+            //    }
+            //}
+
+            if (item.name == "Cube")
+            {
+                roofHealth = item.health;
+            }
+        }
+        houseHealth = (5 * roofHealth).ToString();
+            //houseHealth = (roofHealth + foundationHealth + wall1Health + wall2Health + supportHealth).ToString();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    public void buttonClicked()
+    {
+        GameObject roof = GameObject.FindGameObjectWithTag("roof");
+        GameObject foundation = GameObject.FindGameObjectWithTag("foundation");
+        GameObject wall1 = GameObject.FindGameObjectWithTag("wall");
+        GameObject wall2 = GameObject.FindGameObjectWithTag("extra");
+        GameObject support = GameObject.FindGameObjectWithTag("support");
+
+        if (roof.activeSelf && foundation.activeSelf && wall1.activeSelf && wall2.activeSelf && support.activeSelf)
+        {
+            roofHealth -= 50;
+            houseHealth = (5 * roofHealth).ToString();
+        }
+    }
+
+
+
+}
+
+[System.Serializable]
+public class TextureHealth
+{
+    public string name;
+    public int health;
+}
